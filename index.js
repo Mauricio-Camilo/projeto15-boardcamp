73dba70b2117a752e6ca723597d136e3cdd67f93
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-05-26 15:09:40 -0500

    feat: add route get by id from customers

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000
 
+/* README: CRIAR OS ROUTERS, CONTROLLERS E ALGUNS MIDDLEWARES EM LINS */
+
 app.post("/categories", async (req, res) => {
     const { name } = req.body;
     if (name === "") return res.sendStatus(400);
@@ -73,7 +75,7 @@ app.get("/games", async (req, res) => {
         if (nome !== undefined) {
             // README: DEPOIS APLICAR SQL INJECTION NO LIKE
             const query = await connection.query(`SELECT * FROM games WHERE name LIKE '${nome}%'`);
-            
+
             res.send(query.rows);
         }
         else {
@@ -87,40 +89,40 @@ app.get("/games", async (req, res) => {
     }
 })
 
-app.post("/customers", async (req, res) => {
-    const { name, phone, cpf, birthday} = req.body;
+// app.post("/customers", async (req, res) => {
+//     const { name, phone, cpf, birthday} = req.body;
 
-    // const birthdayRegex = alguma coisa
-    // birthdayRegex.test(birthday);
-    
-    if (name === "" || cpf.length !== 11 
-    || phone.length !== 10) return res.sendStatus(400);
+//     // const birthdayRegex = alguma coisa
+//     // birthdayRegex.test(birthday) RETORNA TRUE OU FALSE;
 
-     // README: FAZER A VALIDAÇÃO DO BIRTHDAY E DO PHONE
+//     if (name === "" || cpf.length !== 11 
+//     || phone.length !== 10) return res.sendStatus(400);
 
-    try {
-        // VALIDAÇÃO DE CPF EXISTENTE, COLOCAR DEPOIS EM UM MIDDLEWARE
-        const cpfclient = await connection.query(
-            `SELECT * FROM customers WHERE cpf=$1`, [cpf]);
-        if (cpfclient.rows.length !== 0) return res.sendStatus(409);
+//      // README: FAZER A VALIDAÇÃO DO BIRTHDAY E DO PHONE
 
-        const query = await connection.query(
-            `INSERT INTO customers ("name", "phone", "cpf", "birthday") 
-            VALUES ($1,$2,$3,$4)`,
-            [name, phone, cpf, birthday]);
-        res.sendStatus(201);
-    }
-    catch (e) {
-        console.log(e);
-        res.status(500).send("Ocorreu um erro ao inserir um cliente");
-    }
-})
+//     try {
+//         // VALIDAÇÃO DE CPF EXISTENTE, COLOCAR DEPOIS EM UM MIDDLEWARE
+//         const cpfclient = await connection.query(
+//             `SELECT * FROM customers WHERE cpf=$1`, [cpf]);
+//         if (cpfclient.rows.length !== 0) return res.sendStatus(409);
+
+//         const query = await connection.query(
+//             `INSERT INTO customers ("name", "phone", "cpf", "birthday") 
+//             VALUES ($1,$2,$3,$4)`,
+//             [name, phone, cpf, birthday]);
+//         res.sendStatus(201);
+//     }
+//     catch (e) {
+//         console.log(e);
+//         res.status(500).send("Ocorreu um erro ao inserir um cliente");
+//     }
+// })
 
 app.get("/customers", async (req, res) => {
     const cpf = req.query.cpf;
     try {
         if (cpf !== undefined) {
-            const query = await connection.query(`SELECT * FROM customers WHERE cpf LIKE '${cpf}%'`);
+            const query = await connection.query(`SELECT * FROM customers WHERE cpf=$1', ${cpf}%'`);
             res.send(query.rows);
         }
         else {
@@ -134,6 +136,24 @@ app.get("/customers", async (req, res) => {
         res.status(500).send("Ocorreu um erro ao exibir os clientes");
     }
 })
+
+app.get("/customers/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const clienteId = await connection.query(`SELECT * FROM customers WHERE id=$1`, [id]);
+        if (clienteId.rows.length === 0) return res.send("Id inexistente").status(404);
+        else return res.send(clienteId.rows);
+
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).send("Ocorreu um erro ao exibir os clientes");
+    }
+})
+
+// FAZER UMA ROTA GET QUE BUSCA POR ID
+
+// FAZER UMA ROTA PUT DOS CLIENTES ID, USAR AS MESMAS VALIDAÇÕES DO POST
 
 
 
